c60827b3b214b795df5430fef9da5b88288a4307
kyle1668 <kyledevinobrien1@gmail.com>
2019-02-22 14:50:56 -0800

    added GET request handler

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 const hackerData = require("./data/hackers.json");
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const app = express();
+const router = express.Router();
+const port = process.env.PORT || 8080;
+
+// Define Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/api", router);
+
+// * Routes
+router.get("/", (req, res) => {
+  res.json({});
+});
+
+// * Routes
+router.get("/:id?", (req, res) => {
+  res.json({});
+});
+
+app.listen(port, () => {
+  console.log(`API Listening On Port ${port}`);
+});
